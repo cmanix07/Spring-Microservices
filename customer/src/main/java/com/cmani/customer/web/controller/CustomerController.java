ce2b2e041a7440fa9ff9b9d50644e1e3f44be26a
cmanix07 <cmani.x07@gmail.com>
2020-10-10 10:13:35 +0900

    Initial commit of Spring-Microservice project V01.

---
+++ customer/src/main/java/com/cmani/customer/web/controller/CustomerController.java
@@ -0,0 +1,26 @@
+package com.cmani.customer.web.controller;
+
+import com.cmani.customer.model.CustomerDto;
+import com.cmani.customer.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    public CustomerService customerService;
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("customerId") UUID id){
+        return new ResponseEntity<CustomerDto>(customerService.getCustomerById(id), HttpStatus.OK);
+    }
+}
